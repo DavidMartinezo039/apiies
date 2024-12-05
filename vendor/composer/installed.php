ce83bf5f9d90582fb3eb3fc57d5510b659b2e6aa
David Martínez Aliaga <158084789+DavidMartinezo039@users.noreply.github.com>
2024-12-05 16:16:20 +0100

    Cambiar

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'b26f7ef6dc44180c7204aa4b22eac15b3de3bb24',
+        'reference' => '54c901361f754032592c63e3b8596b14dbe7f127',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -460,7 +460,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'b26f7ef6dc44180c7204aa4b22eac15b3de3bb24',
+            'reference' => '54c901361f754032592c63e3b8596b14dbe7f127',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
