66095d68168be960ede4687ac6db7cc8b69e86fb
David Martínez Aliaga <158084789+DavidMartinezo039@users.noreply.github.com>
2024-12-16 23:12:22 +0100

    Hacer un crud de producto

--- app/Http/Controllers/Api/V1/ProductController.php
+++ app/Http/Controllers/Api/V1/ProductController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreProductRequest;
 use App\Http\Resources\ProductResource;
+use App\Models\Category;
 use App\Models\Product;
+use Illuminate\Support\Str;
+
 
 class ProductController extends Controller
 {
@@ -15,5 +19,47 @@ class ProductController extends Controller
         return ProductResource::collection($products);
     }
 
+    public function show(Product $product)
+    {
+        return new ProductResource($product);
+    }
+
+    public function store(StoreProductRequest $request)
+    {
+        $data = $request->validated();
+
+        /*
+        if ($request->hasFile('photo')) {
+            $file = $request->file('photo');
+            $name = Str::uuid() . '.' . $file->extension();
+            $file->storeAs('categories', $name, 'public');
+            $data['photo'] = $name;
+        }
+        */
+
+
+        $product = Product::create($data);
+        return new ProductResource($product);
+    }
+
+    public function update(Product $product, StoreProductRequest $request)
+    {
+        $product->update($request->validated());
+
+        return new ProductResource($product);
+    }
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+        //return response(null, Response::HTTP_NO_CONTENT);
+        return response()->noContent();
+    }
+
+    public function list()
+    {
+        return ProductResource::collection(Product::all());
+    }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -14,5 +14,5 @@ Route::get('lists/categories', [CategoryController::class,  'list']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('categories', CategoryController::class);
 
-    Route::get('products', [ProductController::class,  'index']);
+    Route::apiResource('products', ProductController::class);
 });
